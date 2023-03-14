037d2c951800ef52680b6d15891f06ca7276758b
Igor Smirnov <uchetzapis@yahoo.com>
2023-03-14 20:57:12 +0300

    Capital 10

---
+++ src/part_10/2.ts
@@ -0,0 +1,18 @@
+// Наследование от SyntaxError
+
+class FormatError extends SyntaxError { 
+    constructor(message: string) {
+        super(message);
+        this.name = "FormatError";
+    }
+
+}
+
+let err = new FormatError("ошибка форматирования");
+
+console.log(err.message); // ошибка форматирования
+console.log(err.name); // FormatError
+console.log(err.stack); // stack
+
+console.log(err instanceof FormatError); // true
+console.log(err instanceof SyntaxError); // true (потому что наследует от SyntaxError)
